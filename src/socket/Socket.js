0a6a45bd52f203146bba2e8b9efd73d8900986d3
Jonatan <jonatanangel88@gmail.com>
2022-12-12 13:11:32 -0300

    main

--- src/socket/Socket.js
+++ src/socket/Socket.js
@@ -4,8 +4,8 @@ import http from 'http';
 import Review from '../models/Review.model.js';
 import Product from '../models/Product.model.js';
 import { MsgPost, MsgReceived } from '../models/Message.model.js';
-const api = 'https://henry-tech-app.vercel.app';
-// const api = 'http://localhost:3000'
+export const api = 'https://henry-tech-app.vercel.app';
+//  export const api = 'http://localhost:3000'
 export const server = http.createServer(app);
 const io = new SocketServer(server, {
   cors: {
